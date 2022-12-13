d194067da8d8ee250d7501c52c7a253660b8c5bd
cp457 <112082288+cp457@users.noreply.github.com>
2022-12-13 10:50:46 +0100

    Add files via upload

---
+++ String Handling/src/Tester.java
@@ -0,0 +1,17 @@
+public class Tester {
+    public static void main(String...Args){
+
+        String s1 = "Hello";
+        String s2 = "How are you?";
+
+        s1.charAt(0);
+        s1.charAt(1);
+        s2.charAt(10);
+        s2.charAt(11);
+
+        char[] charSequence = {'H', 'e', 'u', '?'};
+
+        System.out.println(charSequence);
+
+    }
+}
